63a58db58c853ff468bcf7e257932fd835afc46d
pdunnigan <patrick.dunnigan@cloudsidekick.com>
2014-09-22 16:59:24 -0400

    widened name field

--- initDB.sql
+++ initDB.sql
@@ -6,7 +6,7 @@ USE petclinic;
 CREATE TABLE IF NOT EXISTS vets (
   id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30),
-  last_name VARCHAR(30),
+  last_name VARCHAR(64),
   INDEX(last_name)
 ) engine=InnoDB;
 
